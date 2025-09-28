c7f1e6b198bdd00777a0ebb0c2957832a76c16e7
itskrixhna32 <itskrixhna32@gmail.com>
2025-09-28 17:52:46 +0000

    Declare variables within different code blocks (enclosed by curly braces) and test their accessibility within and outside those blocks.

--- number_positive_negative_zero.c
+++ number_positive_negative_zero.c
@@ -18,7 +18,7 @@ int main()
             zeros++;
 
         printf("Do you want to enter another number? (y/n): ");
-        scanf(" %c", &choice); // space before %c to ignore newline
+        scanf(" %c", &choice);
 
     } while (choice == 'y' || choice == 'Y');
 
